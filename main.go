8f137d34e830818d8df38fc9f76d4956f0b855e6
Krishna Sundarram <krishna.sundarram@gmail.com>
2015-06-10 10:54:53 +0530

    Add http endpoints for shortening and redirecting

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math/rand"
+	"net/http"
 	"strings"
 	"time"
 
@@ -40,7 +41,37 @@ func redisSet(tablename, key, value string) error {
 }
 
 func redisGet(tablename, key string) (string, error) {
-	redis.String(c.Do("HGET", tablename, key))
+	return redis.String(c.Do("HGET", tablename, key))
+}
+
+func shorten(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r.Form, r.PostForm)
+	longurl := r.Form.Get("longurl")
+	if len(longurl) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	existing, err := redisGet(longmap, longurl)
+	fmt.Println(existing, err)
+	if err == nil {
+		w.Write([]byte(existing))
+		return
+	}
+	shorturl := getShortURL()
+	redisSet(longmap, longurl, shorturl)
+	redisSet(shortmap, shorturl, longurl)
+	w.Write([]byte(shorturl))
+}
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	longurl, err := redisGet(shortmap, r.URL.Path[1:len(r.URL.Path)])
+	fmt.Println(r.URL.Path, err)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	fmt.Println(longurl)
 }
 
 func main() {
@@ -48,11 +79,16 @@ func main() {
 	adjectives, _ = readWords("adjectives3.txt")
 	rand.Seed(time.Now().UnixNano())
 
-	c, err := redis.Dial("tcp", ":6379")
+	var err error
+	c, err = redis.Dial("tcp", ":6379")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer c.Close()
+
+	http.HandleFunc("/shorten", shorten)
+	http.HandleFunc("/", redirect)
+	http.ListenAndServe(":9091", nil)
 
 }
